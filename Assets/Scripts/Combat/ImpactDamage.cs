0f76bef9b64c03e07a3597d82c9935e7ee87ddb8
Jan Schwien <jan5366@gmail.com>
2018-12-01 22:16:24 +0100

    fixed friendly fire, added alien laser

--- Assets/Scripts/Combat/ImpactDamage.cs
+++ Assets/Scripts/Combat/ImpactDamage.cs
@@ -5,6 +5,7 @@ namespace Combat
     public class ImpactDamage : MonoBehaviour
     {
         public bool DamagePlayer = false;
+        public bool DamageEnemies = true;
         public float Damage = 10F;
         public Transform ImpactPrefab;
 
@@ -13,7 +14,7 @@ namespace Combat
             Destructible otherUnit = other.GetComponent<Destructible>();
             
             // ignore invalid units
-            if (otherUnit == null || (otherUnit.IsPlayer && !DamagePlayer) || otherUnit.IsDead())
+            if (otherUnit == null || (otherUnit.IsPlayer && !DamagePlayer) || (!otherUnit.IsPlayer && !DamageEnemies) || otherUnit.IsDead())
                 return;
             
             otherUnit.hit(Damage);
