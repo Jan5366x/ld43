b0c36b7e0335d08575d85bf1ce43cd374173fd80
Hendrik Rombach <h.rombach1@gmail.com>
2018-12-01 20:02:15 +0100

    Fix HUD script

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,4 +1,5 @@
-﻿using Combat;
+﻿using System;
+using Combat;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,30 +15,44 @@ public class HUD : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        _healthSlider = GameObject.FindWithTag("HealthBar").GetComponent<Slider>();
-        _shieldSlider = GameObject.FindWithTag("ShieldBar").GetComponent<Slider>();
-        _energySlider = GameObject.FindWithTag("EnergyBar").GetComponent<Slider>();
-
-        _player = GameObject.FindWithTag("Player");
-        _playerDestruct = _player.GetComponent<Destructible>();
-        _playerWeapon = _player.GetComponentInChildren<PlayerWeapon>();
-
-        //init sliders
-        _healthSlider.maxValue = _playerDestruct.MaxArmor;
-        _healthSlider.value = _playerDestruct.CurrentArmor;
-
-        _shieldSlider.maxValue = _playerDestruct.MaxShield;
-        _shieldSlider.value = _playerDestruct.CurrentShield;
-
-        _energySlider.maxValue = _playerWeapon.MaxEnergy;
-        _energySlider.value = _playerWeapon.CurrentEnergy;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!InitPlayer()) return;
+
         _healthSlider.value = _playerDestruct.CurrentArmor;
         _shieldSlider.value = _playerDestruct.CurrentShield;
         _energySlider.value = _playerWeapon.CurrentEnergy;
+    }
+
+    private bool InitPlayer()
+    {
+        if (null == _player)
+        {
+            _healthSlider = GameObject.FindWithTag("HealthBar").GetComponent<Slider>();
+            _shieldSlider = GameObject.FindWithTag("ShieldBar").GetComponent<Slider>();
+            _energySlider = GameObject.FindWithTag("EnergyBar").GetComponent<Slider>();
+
+            _player = GameObject.FindWithTag("Player");
+
+            if (null == _player) return false;
+
+            _playerDestruct = _player.GetComponent<Destructible>();
+            _playerWeapon = _player.GetComponentInChildren<PlayerWeapon>();
+
+            //init sliders
+            _healthSlider.maxValue = _playerDestruct.MaxArmor;
+            _healthSlider.value = _playerDestruct.CurrentArmor;
+
+            _shieldSlider.maxValue = _playerDestruct.MaxShield;
+            _shieldSlider.value = _playerDestruct.CurrentShield;
+
+            _energySlider.maxValue = _playerWeapon.MaxEnergy;
+            _energySlider.value = _playerWeapon.CurrentEnergy;
+        }
+
+        return null != _player;
     }
 }
