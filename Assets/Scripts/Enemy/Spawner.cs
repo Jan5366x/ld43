ca9e5e24fe05d6d53008c983fc4f86b97eda5e16
Kormarun <kormarun@gmail.com>
2018-12-01 16:54:25 +0100

    Added Enemy Spawner + refactoring

---
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform Prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("SpawnDelayed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnDelayed()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject player = GameObject.FindWithTag("Player");
+        
+        Instantiate(Prefab, player.transform.position + new Vector3(12, 0, 0), Quaternion.identity, transform);
+    }
+}
