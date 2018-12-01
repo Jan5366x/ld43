5581a1b8412097b4785b65077476e802218bec48
Jan Schwien <jan5366@gmail.com>
2018-12-01 17:58:17 +0100

    work on items and projectiles

---
+++ Assets/Scripts/Combat/ImpactDamage.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Combat
+{
+    public class ImpactDamage : MonoBehaviour
+    {
+        public bool DamagePlayer = false;
+        public bool DamageEnemies = true;
+        public float Damage = 10F;
+        
+    }
+}
