a4df739c5e11ae6ede563b4c4b9c03120fb13cce
Hendrik Rombach <h.rombach1@gmail.com>
2018-12-02 13:46:39 +0100

    (Hopefully) fix NullReference in HUD script

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,14 +1,19 @@
-﻿using Combat;
+﻿using System;
+using Combat;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
+    // UI Objects
     private Slider _healthSlider;
     private Slider _shieldSlider;
-    private GameObject _player;
-    private Destructible _playerDestruct;
     private Slider _energySlider;
+
+    private WeaponPanel _playerWeaponPanel;
+
+    // player state
+    private Destructible _playerDestruct;
     private PlayerWeapon _playerWeapon;
 
     // Use this for initialization
@@ -19,39 +24,48 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!InitPlayer()) return;
+        if (!HasContext()) return;
 
         _healthSlider.value = _playerDestruct.CurrentArmor;
         _shieldSlider.value = _playerDestruct.CurrentShield;
         _energySlider.value = _playerWeapon.CurrentEnergy;
     }
 
-    private bool InitPlayer()
+    private bool HasContext()
     {
-        if (null == _player)
+        if (null == _playerDestruct || null == _playerWeapon)
         {
-            _healthSlider = GameObject.FindWithTag("HealthBar").GetComponent<Slider>();
-            _shieldSlider = GameObject.FindWithTag("ShieldBar").GetComponent<Slider>();
-            _energySlider = GameObject.FindWithTag("EnergyBar").GetComponent<Slider>();
-
-            _player = GameObject.FindWithTag("Player");
-
-            if (null == _player) return false;
-
-            _playerDestruct = _player.GetComponent<Destructible>();
-            _playerWeapon = _player.GetComponentInChildren<PlayerWeapon>();
-
-            //init sliders
-            _healthSlider.maxValue = _playerDestruct.MaxArmor;
-            _healthSlider.value = _playerDestruct.CurrentArmor;
-
-            _shieldSlider.maxValue = _playerDestruct.MaxShield;
-            _shieldSlider.value = _playerDestruct.CurrentShield;
-
-            _energySlider.maxValue = _playerWeapon.MaxEnergy;
-            _energySlider.value = _playerWeapon.CurrentEnergy;
+            try { InitContext(); }
+            catch (NullReferenceException e)
+            {
+                Debug.Log(e);
+                return false;
+            }
         }
 
-        return null != _player;
+        return true;
+    }
+
+    private void InitContext()
+    {
+        _healthSlider = GameObject.FindWithTag("HealthBar").GetComponent<Slider>();
+        _shieldSlider = GameObject.FindWithTag("ShieldBar").GetComponent<Slider>();
+        _energySlider = GameObject.FindWithTag("EnergyBar").GetComponent<Slider>();
+        _playerWeaponPanel = GameObject.FindWithTag("PlayerWeaponPanel").GetComponent<WeaponPanel>();
+
+        GameObject player = GameObject.FindWithTag("Player");
+
+        _playerDestruct = player.GetComponent<Destructible>();
+        _playerWeapon = player.GetComponentInChildren<PlayerWeapon>();
+
+        //init sliders
+        _healthSlider.maxValue = _playerDestruct.MaxArmor;
+        _healthSlider.value = _playerDestruct.CurrentArmor;
+
+        _shieldSlider.maxValue = _playerDestruct.MaxShield;
+        _shieldSlider.value = _playerDestruct.CurrentShield;
+
+        _energySlider.maxValue = _playerWeapon.MaxEnergy;
+        _energySlider.value = _playerWeapon.CurrentEnergy;
     }
 }
---
+++ Assets/Scripts/UI/WeaponPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPanel : MonoBehaviour
+{
+    enum WeaponTypes
+    {
+        SingleShot
+    }
+
+    List<GameObject> icons = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        icons.Insert((int)WeaponTypes.SingleShot, GameObject.FindWithTag("SingleShotIcon"));
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
