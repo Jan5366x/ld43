37284a8892a331adb9688df7e6a60325a6df75f8
Kormarun <kormarun@gmail.com>
2018-12-02 14:44:25 +0100

    SwapWeaponsOnPickup

--- Assets/Scripts/Pickups/PickupItem.cs
+++ Assets/Scripts/Pickups/PickupItem.cs
@@ -14,28 +14,59 @@ public class PickupItem : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        var d = other.GetComponent<Destructible>();
-        if (!d) return;
+        if (!isPlayer(other)) return;
 
-        if (!d.IsDead() && d.IsPlayer)
-        {
-            d.CurrentShield = Mathf.Clamp(d.CurrentShield + ShieldDelta, 0, d.MaxShield);
-            d.CurrentArmor = Mathf.Clamp(d.CurrentArmor + ArmorDelta, 0, d.MaxArmor);
-        }
-
-        var w = other.GetComponentInChildren<PlayerWeapon>();
-        if (!w) return;
-
-        w.CurrentEnergy = Mathf.Clamp(w.CurrentEnergy + EnergyDelta, 0, w.MaxEnergy);
+        updateWeapon(other);
+        updateShields(other);
+        updateArmor(other);
+        updateEnergy(other);
+        updateScore(other);
 
         Destroy(gameObject);
+    }
 
+    private void updateWeapon(Component other)
+    {
         if (!Item) return;
-        
+
         var swap = other.GetComponentInChildren<SwapWeapon>();
         if (swap)
         {
             swap.Swap(Item);
         }
+    }
+
+    private void updateShields(Component other)
+    {
+        var destructible = other.GetComponent<Destructible>();
+        destructible.CurrentShield = Mathf.Clamp(destructible.CurrentShield + ShieldDelta, 0, destructible.MaxShield);
+    }
+
+    private void updateArmor(Component other)
+    {
+        var destructible = other.GetComponent<Destructible>();
+        destructible.CurrentArmor = Mathf.Clamp(destructible.CurrentArmor + ArmorDelta, 0, destructible.MaxArmor);
+    }
+
+    private void updateEnergy(Component other)
+    {
+        var w = other.GetComponentInChildren<PlayerWeapon>();
+        if (!w) return;
+
+        w.CurrentEnergy = Mathf.Clamp(w.CurrentEnergy + EnergyDelta, 0, w.MaxEnergy);
+    }
+
+    private void updateScore(Component other)
+    {
+        var score = other.GetComponent<ScoreCounter>();
+        score.Score = Mathf.Max(0, PointsDelta);
+    }
+
+    private bool isPlayer(Component other)
+    {
+        var destructible = other.GetComponent<Destructible>();
+        if (!destructible) return false;
+
+        return !destructible.IsDead() && destructible.IsPlayer;
     }
 }
