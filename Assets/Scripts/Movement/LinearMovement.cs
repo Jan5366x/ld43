a49a63aa8f4dac4951ab3fdc236fd9edd986c6fb
Kormarun <kormarun@gmail.com>
2018-12-03 13:13:40 +0100

    More Pewpew

---
+++ Assets/Scripts/Movement/LinearMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovement : MonoBehaviour
+{
+    public float Angle;
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion quat = Quaternion.AngleAxis(Angle, Vector3.forward);
+        transform.rotation = quat;
+        Vector3 vec = quat * Vector3.right;
+        transform.Translate(Time.deltaTime * vec * Speed);
+    }
+}
