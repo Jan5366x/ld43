7de8388ba11888e0ec89e095777bf5ec51e2e3c5
Kormarun <kormarun@gmail.com>
2018-12-04 00:24:51 +0100

    More gameplay balance & Death screen

---
+++ Assets/Scripts/UI/ScoreUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var text = GetComponent<TextMeshPro>();
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (!player) return;
+        var score = player.GetComponent<ScoreCounter>();
+        if (!score) return;
+        if (text)
+        {
+            text.text = "Score: " + score.Score;
+        }
+    }
+}
