6ab8218612ec34ef0e76c961a3641a8543b26882
Kormarun <kormarun@gmail.com>
2018-12-03 17:20:56 +0100

    Pausemenu

---
+++ Assets/Scripts/UI/PauseScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScene : MonoBehaviour
+{
+    private bool _paused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Toggle();
+        }
+        else
+        {
+
+//            if (_paused == false && Input.GetAxis("Cancel") > 0.8)
+//            {
+//                Toggle();
+//            }
+//            else if (_paused && Input.GetAxis("Cancel") < 0.2)
+//            {
+//                Toggle();
+//            }
+        }
+    }
+
+    void Toggle()
+    {
+        _paused = !_paused;
+        Time.timeScale = _paused ? 1f : 0f;
+        GetComponent<Image>().enabled = !_paused;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(!_paused);
+        }  
+    }
+}
