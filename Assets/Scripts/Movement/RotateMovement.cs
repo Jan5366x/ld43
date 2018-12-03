0842e72b47a67dba28f3da2b6a638d053ff3165b
Kormarun <kormarun@gmail.com>
2018-12-03 21:51:22 +0100

    More aliens and stuff

---
+++ Assets/Scripts/Movement/RotateMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMovement : MonoBehaviour
+{
+    public float AngularVelocity = 5;
+    private float _angle = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _angle += AngularVelocity * Time.deltaTime;
+        transform.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
+    }
+}
